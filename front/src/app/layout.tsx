2e6777506b306b72d59166751dceeb4f27f2664e
rere <regis-graptin@sfr.fr>
2023-09-20 12:06:12 +0200

    feat: create provider folder

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Providers } from './providers'
+
+import { Providers as ChakraProvider } from '../providers/chakraProvider'; 
+import { Providers as WalletProvider } from '../providers/walletProvider'; 
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +20,11 @@ export default function RootLayout({
   return (
         <html lang="en">
           <body className={inter.className}>
-            <Providers>
-              {children}
-            </Providers>
+            <ChakraProvider>
+              <WalletProvider>
+                {children}
+              </WalletProvider>
+            </ChakraProvider>
           </body>
         </html>
       
